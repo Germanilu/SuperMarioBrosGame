cf59ea1eb501ce2ea6e8a7aa5fbda42953b6c89f
Germanilu <lucianogermani93@gmail.com>
2022-06-18 13:41:49 +0200

    Kick off SuperMarioBros

---
+++ js/game.js
@@ -0,0 +1,13 @@
+kaboom({
+    global: true,
+    fullscreen: true,
+    scale:1,
+    debug: true,
+    clearColor: [0,0,0,1], //BG color set to Black
+})
+
+screen("game", () => {
+
+})
+
+start("game")
