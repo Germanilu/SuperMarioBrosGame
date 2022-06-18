58d7f54057a4bcacdb6c974b7c562b8ce10ab762
Germanilu <lucianogermani93@gmail.com>
2022-06-18 16:05:36 +0200

    edit map

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 kaboom({
     global: true,
     fullscreen: true,
-    scale:1,
+    scale:1.5,
     debug: true,
     clearColor: [0,0,0,1], //BG color set to Black
 })
@@ -17,7 +17,7 @@ loadSprite('mario','/img/mario.png') //Mario
 loadSprite('mushroom','/img/mushroom.png') //Mushroom
 loadSprite('surprise','/img/surprise.png') //Surprise Block
 loadSprite('unboxed','/img/unboxed.png') //unBoxed
-loadSprite('pipe-top-left','/img/piepe-top-left.png') //Pipe
+loadSprite('pipe-top-left','/img/pipe-top-left.png') //Pipe
 loadSprite('pipe-top-right','/img/pipe-top-right.png') //Pipe
 loadSprite('pipe-bottom-left','/img/pipe-bottom-left.png') //Pipe
 loadSprite('pipe-bottom-right','/img/pipe-bottom-right.png') //Pipe
@@ -34,18 +34,31 @@ scene("game", () => {
         '                                 ',
         '                                 ',
         '                                 ',
+        '       %  =*=%=                        ',
         '                                 ',
         '                                 ',
-        '                                 ',
-        '                                 ',
-        '                                 ',
+        '                      -+          ',
+        '                ^   ^ ()          ',
         '=========================  ======',
     ]
 
     const levelCfg = {
         width: 20,
         height: 20,
-        '=': [sprite('block',solid())] //Equal to the block img
+        '=': [sprite('block',solid())], //Equal to the block img
+        "$": [sprite('coin')],
+        '%': [sprite('surprise'), solid(), 'coin-surprise'],
+        '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
+        '}': [sprite('unboxed'),solid()],
+        '(': [sprite('pipe-bottom-left'),solid(), scale(0.5)],
+        ')': [sprite('pipe-bottom-right'),solid(), scale(0.5)],
+        '-': [sprite('pipe-top-left'),solid(), scale(0.5)],
+        '+': [sprite('pipe-top-right'),solid(), scale(0.5)],
+        '^': [sprite('evil-shroom'),solid()],
+        '#': [sprite('mushroom'),solid()],
+        
+
+
     }
 
     const gameLevel = addLevel(map, levelCfg)
